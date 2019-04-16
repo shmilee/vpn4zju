7ecee558102bdc684b17a2af41288c5d3cbc7aa8
shmilee <shmilee.zju@gmail.com>
2019-04-16 20:03:28 +0800

    wicd-postconnect.sh run zjuwlan as root
    username, password set by keyring and vpn4zju

--- wicd-postconnect.sh
+++ wicd-postconnect.sh
@@ -6,6 +6,9 @@ connection_type="$1"
 essid="$2"
 bssid="$3"
 
+#echo ${connection_type} > /tmp/wicd-ZJUWLAN.log
+#echo ${essid} >> /tmp/wicd-ZJUWLAN.log
 if [[ "${connection_type}" == "wireless" && "${essid}" == 'ZJUWLAN' ]]; then
     zjuwlan login
+    #zjuwlan login >> /tmp/wicd-ZJUWLAN.log
 fi
--- zjuwlan.py
+++ zjuwlan.py
@@ -3,13 +3,13 @@
 
 # Copyright (c) 2019 shmilee
 
+import os
+import sys
 import argparse
+import configparser
 import getpass
 import keyring
 import requests
-
-username = keyring.get_password('zjuvpn', 'username')
-password = keyring.get_password('zjuvpn', 'password')
 
 post_url = 'https://net.zju.edu.cn/include/auth_action.php'
 post_headers = {
@@ -20,37 +20,92 @@ post_headers = {
     'X-Requested-With': 'XMLHttpRequest',
     # 'Cookie': 'login=xxx'
 }
-post_login_data = dict(
-    action='login',
-    username=username,
-    password=password,
-    ac_id=3,
-    # user_ip='',
-    # nas_ip='',
-    # user_mac='',
-    save_me=0,
-    ajax=1,
-)
-post_logout_data = dict(
-    action='logout',
-    username=username,
-    password=password,
-    ajax=1,
-)
+
+
+def get_v_user_pass():
+    xl2tpdconf = '/etc/xl2tpd/xl2tpd.conf'
+    username, password = None, None
+    try:
+        print("Use 'username', 'password' from %s ..." % xl2tpdconf)
+        config = configparser.ConfigParser(inline_comment_prefixes=('#', ';'))
+        config.read(xl2tpdconf)
+        auth_file = config['global']['auth file']
+        username = config['lac ZJU']['name']
+        if username.find('@'):
+            username = username[:username.find('@')]
+    except (Exception, KeyError) as e:
+        print(e)
+    if not username:
+        print("Failed to get ['lac ZJU']['name'] in %s!" % xl2tpdconf)
+    else:
+        try:
+            with open(auth_file) as auth:
+                for line in auth.readlines():
+                    if line.startswith(username):
+                        password = line[line.find('*')+1:line.rfind('*')]
+                        password = password.strip()
+        except (Exception, KeyError) as e:
+            print(e)
+        if not password:
+            print("Failed to get 'password' in %s!" % auth_file)
+    return username, password
+
+
+def get_k_user_pass():
+    username, password = None, None
+    try:
+        print("Use 'username', 'password' from keyring ...")
+        username = keyring.get_password('zjuvpn', 'username')
+        password = keyring.get_password('zjuvpn', 'password')
+    except (Exception, RuntimeError) as e:
+        print(e)
+    if not username:
+        print("Failed to get 'username' by keyring!")
+    if not password:
+        print("Failed to get 'password' by keyring!")
+    return username, password
+
+
+def get_post_data(action):
+    if getpass.getuser() == 'root':
+        username, password = get_v_user_pass()
+    else:
+        username, password = get_k_user_pass()
+    if not(username and password):
+        sys.exit(1)
+    if action == 'login':
+        return dict(
+            action='login',
+            username=username,
+            password=password,
+            ac_id=3,
+            # user_ip='',
+            # nas_ip='',
+            # user_mac='',
+            save_me=0,
+            ajax=1)
+    elif action == 'logout':
+        return dict(
+            action='logout',
+            username=username,
+            password=password,
+            ajax=1)
 
 
 def main(args):
-    if args.action == 'conf':
-        username = getpass.getpass("Username in 'zjuvpn': ")
+    if args.action == 'kconf':
+        username = input("Username in 'zjuvpn': ")
         password = getpass.getpass(
             "Password for '%s' in 'zjuvpn': " % username)
         keyring.set_password("zjuvpn", "username", username)
         keyring.set_password("zjuvpn", "password", password)
         print("Done.")
+    elif args.action == 'vconf':
+        os.system('sudo vpn4zju -cfg')
     elif args.action == 'login':
         print('Logging in ZJUWLAN ...')
         response = requests.post(
-            post_url, data=post_login_data, headers=post_headers)
+            post_url, data=get_post_data('login'), headers=post_headers)
         response.encoding = 'utf-8'
         if response.text.startswith('login_ok'):
             print('Login successful.')
@@ -59,7 +114,7 @@ def main(args):
     elif args.action == 'logout':
         print('Logging out ZJUWLAN ...')
         response = requests.post(
-            post_url, data=post_logout_data, headers=post_headers)
+            post_url, data=get_post_data('logout'), headers=post_headers)
         response.encoding = 'utf-8'
         if response.text.startswith('网络已断开'):
             print('Logout successful.')
@@ -70,12 +125,15 @@ def main(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description="Login/Logout ZJUWLAN v0.1 by shmilee@zju.edu.cn",
+        epilog="Password stored by keyring is used for current USER,\n"
+        "and stored by vpn4zju(ppp) is used for root USER.",
         formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument('action', metavar='Action',
                         nargs='?', default='login',
-                        choices=['login', 'logout', 'conf'],
+                        choices=['login', 'logout', 'kconf', 'vconf'],
                         help='%(choices)s\n'
                              'default is %(default)s\n'
-                             'conf: set username password')
+                             'kconf: set username password by keyring\n'
+                             'vconf: set username password by vpn4zju')
     args = parser.parse_args()
     main(args)
